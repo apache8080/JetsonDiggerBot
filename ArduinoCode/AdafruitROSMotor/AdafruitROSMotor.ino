6d71ee376d058b203fad4fbe325c9668eba16c35
apache8080 <desai.rishi1@gmail.com>
2015-07-10 23:04:21 +0000

    finished arduino drive code waiting for motor controller

--- ArduinoCode/AdafruitROSMotor/AdafruitROSMotor.ino
+++ ArduinoCode/AdafruitROSMotor/AdafruitROSMotor.ino
@@ -1,14 +1,6 @@
-
-#define USE_USBCON
-#if (ARDUINO >= 100)
- #include <Arduino.h>
-#else
- #include <WProgram.h>
-#endif
-
-#include <Servo.h> 
 #include <ros.h>
-#include <std_msgs/UInt16.h>
+#include <diggerbot/DriveVals.h>
+#include <std_msgs/String.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
@@ -16,39 +8,91 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
+Adafruit_DCMotor *leftMotor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *leftMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *rightMotor1 = AFMS.getMotor(3);
+Adafruit_DCMotor *rightMotor2 = AFMS.getMotor(4);
 
 ros::NodeHandle  nh;
+std_msgs::String msg;
+ros::Publisher pub("arduinoval", &msg);
+boolean leftIsReverse, rightIsReverse;
 
-void servo_cb( const std_msgs::UInt16& cmd_msg){
-  servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
-  digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+void drive_motors( const diggerbot::DriveVals& cmd_msgs){
+  int leftSpeed = cmd_msgs.speeds[0];
+  int rightSpeed = cmd_msgs.speeds[1];
+  if(leftSpeed<0){
+    leftMotor1->setSpeed(abs(leftSpeed));
+    leftMotor2->setSpeed(abs(leftSpeed));
+    leftMotor1->run(BACKWARD);
+    leftMotor2->run(BACKWARD);
+    leftIsReverse = true;
+  }else{
+    leftMotor1->setSpeed(abs(leftSpeed));
+    leftMotor2->setSpeed(abs(leftSpeed));
+    leftMotor1->run(FORWARD);
+    leftMotor2->run(FORWARD);
+    leftIsReverse = false;
+  }
+  if(rightSpeed<0){
+    rightMotor1->setSpeed(abs(rightSpeed));
+    rightMotor2->setSpeed(abs(rightSpeed));
+    rightMotor1->run(BACKWARD);
+    rightMotor2->run(BACKWARD);
+    rightIsReverse=true;
+  }else{
+    rightMotor1->setSpeed(abs(rightSpeed));
+    rightMotor2->setSpeed(abs(rightSpeed));
+    rightMotor1->run(FORWARD);
+    rightMotor2->run(FORWARD);
+    rightIsReverse=false;
+  }
 }
 
 
-ros::Subscriber<std_msgs::UInt16> sub("servo", servo_cb);
+ros::Subscriber<diggerbot::DriveVals> sub("drivevals", drive_motors);
+
 
 void setup(){
-  pinMode(13, OUTPUT);
+  //pinMode(13, OUTPUT);
 
   nh.initNode();
   nh.subscribe(sub);
-  
-  servo.attach(9); //attach it to pin 9
+  nh.advertise(pub);
+  //servo.attach(9); //attach it to pin 9
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
+  leftMotor1->setSpeed(0);
+  leftMotor1->run(FORWARD);
+  leftMotor2->setSpeed(0);
+  leftMotor2->run(FORWARD);
+  rightMotor1->setSpeed(0);
+  rightMotor1->run(FORWARD);
+  rightMotor2->setSpeed(0);
+  rightMotor2->run(FORWARD);
   // turn on motor
-  myMotor->run(RELEASE);
+  leftMotor1->run(RELEASE);
+  leftMotor2->run(RELEASE);
+  rightMotor1->run(RELEASE);
+  rightMotor2->run(RELEASE);
 }
 
 void loop(){
+  if(leftIsReverse && rightIsReverse){
+    msg.data = "Reverse";
+    pub.publish(&msg);
+  }else if(!leftIsReverse && rightIsReverse){
+    msg.data = "Right Turn";
+    pub.publish(&msg);
+  }else if(leftIsReverse && !rightIsReverse){
+    msg.data = "Left Turn";
+    pub.publish(&msg);
+  }else{
+    msg.data = "Forward";
+    pub.publish(&msg);
+  }
   nh.spinOnce();
   delay(1);
 }

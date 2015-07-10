d9f1865ac3d05bfec691f55d5b6b194031945052
apache8080 <desai.rishi1@gmail.com>
2015-07-10 00:16:01 +0000

    finished all drive communication code in python with ROS all that is left is adding Arduino Code

--- diggerbot/scripts/joystick.py
+++ diggerbot/scripts/joystick.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
-
-#try:
 from sensor_msgs.msg import Joy
-#from std_msgs.msg import Int16MultiArray
-#from diggerbot.msg import JoyVals
-#except ImportError:
- #   from joy.msg import Joy
+from diggerbot.msg import JoyVals
 
 def main():
 	rospy.init_node('joystick')
@@ -19,18 +14,17 @@ def main():
 	
 def joyCallback(joy):
 	buttons=joy.buttons
-	#joy_value_pub = rospy.Publisher("joyval", JoyVals)
-	#print buttons
-	#joy_vals=JoyVals
+	joy_value_pub = rospy.Publisher('joyval', JoyVals)
+	joy_vals=JoyVals()
 	joy_buttons_pressed=[]
 	for b in range(len(buttons)):
 		if buttons[b]==1:
 			print 'button: '+str(b)+' was pressed'
 			joy_buttons_pressed.append(b)
 	print 'Joy1 Y-axis: '+str(joy.axes[1])+' Joy2 X-axis: '+str(joy.axes[2])
-	#joy_vals.button_vals = joy_buttons_pressed	
-	#joy_vals.joystick_vals = [joy.axes[1],joy.axes[2]]
-	#joy_value_pub.publish(joy_vals)
+	joy_vals.button_vals = joy_buttons_pressed	
+	joy_vals.joystick_vals = [joy.axes[1],joy.axes[2]]
+	joy_value_pub.publish(joy_vals)
 
 if __name__ == '__main__':
 	main()
